fbafd099a8a7a7e7fae1890bbd50aa653798bbd2
Seblecaribou <morin.seb.89@gmail.com>
2020-09-27 15:32:14 +0200

    Added music

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let titleScreen = true;
 let gameisover = false;
+const pushItToTheLimit = document.getElementsByTagName("audio")[0];
 const TitleScreenWindow = document.getElementsByClassName("TitleScreen")[0];
 const GameWindow = document.getElementsByClassName("Game")[0];
 const GameOver = document.getElementsByClassName("GameOver")[0];
@@ -8,6 +9,8 @@ const Obstacle = document.getElementById("Obstacle");
 const scoreSpan = document.getElementById("scoreSpan");
 const finalCounter = document.getElementsByTagName("p")[1];
 let counter = 0;
+
+pushItToTheLimit.volume = 0.2;
 
 function StartOrJump() {
   if (titleScreen) {
